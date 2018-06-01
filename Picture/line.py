cb6f5a878c884fb2d2004c6624c517a839aaf704
gasper <gasper.hribarrr@gmail.com>
2018-06-01 14:40:08 +0200

    square&number detection

--- Picture/line.py
+++ Picture/line.py
@@ -28,6 +28,27 @@ class Line:
             return 100
         return float("%.2f" % (up / down))
 
+    @staticmethod
+    def get_coefs(line):
+        a = (line.y1 - line.y2)
+        b = (line.x2 - line.x1)
+        c = (line.x1 * line.y2 - line.x2 * line.y1)
+        return a, b, -c
+
+    @staticmethod
+    def get_intersection(line1, line2):
+        coefs1 = Line.get_coefs(line1)
+        coefs2 = Line.get_coefs(line2)
+        d = coefs1[0] * coefs2[1] - coefs1[1] * coefs2[0]
+        dx = coefs1[2] * coefs2[1] - coefs1[1] * coefs2[2]
+        dy = coefs1[0] * coefs2[2] - coefs1[2] * coefs2[0]
+        if d != 0:
+            x = dx / d
+            y = dy / d
+            return round(x), round(y)
+        else:
+            return False
+
 
 class Orientation(Enum):
     Horizontal = 0
@@ -35,4 +56,4 @@ class Orientation(Enum):
 
 
 if __name__ == '__main__':
-    Line(0, 20, -11, 3)
+    Line.get_intersection(Line(0, 20, 11, 3), Line(3, 2, 1, 0))
--- Picture/lineDetection.py
+++ Picture/lineDetection.py
@@ -3,6 +3,8 @@ import numpy as np
 import operator
 from Picture.line import Line
 from Picture.line import Orientation
+from Picture.numberSquare import NumberSquare
+from Picture.skLearn import classify, setup_classifier
 
 
 class Image:
@@ -10,13 +12,15 @@ class Image:
         self.path = path
         self.img = cv2.imread(path)
         self.height, self.width, self.channels = self.img.shape
+        self.grid_lines = self._get_main_lines()
+        self.squares = self._get_field_squares()
 
     def _get_hough_lines(self):
         gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)  # Convert the img to gray scale
         edges = cv2.Canny(gray, 50, 150, apertureSize=3)  # Apply edge detection method on the image
         return cv2.HoughLines(edges, 1, np.pi / 180, 170)
 
-    def _get_lines(self):
+    def _get_all_lines(self):
         lines = {}
         lines[Orientation.Horizontal] = []
         lines[Orientation.Vertical] = []
@@ -38,13 +42,13 @@ class Image:
     def _get_padding(self):
         return self.width // 18  # Based on the test pictures this is the best ratio
 
-    def line_minimization(self):
+    def _get_main_lines(self):
         """
-        Keeps only the lines that are needed
+        Keeps only the lines that are needed (without the noise)
         :return: new Dictionary with fewer lines
         """
         max_padding = self._get_padding()  # minimum line distance (if lower the line will be thrown away)
-        lines = self._get_lines()
+        lines = self._get_all_lines()
 
         # all sorted horizontal lines
         horizontal_lines = lines[Orientation.Horizontal]
@@ -69,17 +73,69 @@ class Image:
 
         return {Orientation.Horizontal: new_horizontal_lines, Orientation.Vertical: new_vertical_lines}
 
+    def _get_field_squares(self):
+        squares = []
+        horizontals = self.grid_lines[Orientation.Horizontal]
+        verticals = self.grid_lines[Orientation.Vertical]
+        for h, h_line in enumerate(horizontals):
+            squares.append([])
+            for v, v_line in enumerate(verticals):
+                try:  # if there is no intersection it will return false, which is not iterable
+                    x, y = Line.get_intersection(h_line, v_line)  # causes TypeError if False
+                    if v < len(verticals) - 1 and h < len(horizontals) - 1:
+                        squares[h].append(NumberSquare(v, h, (x, y)))
+                    if v > 0 and h < len(horizontals) - 1:  # gives the point to the left square
+                        squares[h][v - 1].add_point(x, y)  # adds the point to the previous "square"
+                    if h > 0 and v < len(verticals) - 1:  # gives the point to the top square
+                        squares[h - 1][v].add_point(x, y)
+                    if h > 0 and v > 0:  # gives the point to the top left square
+                        squares[h - 1][v - 1].add_point(x, y)
+                except TypeError:
+                    print("No intersection between those lines!")
+        return squares
+
     def draw_lines(self):
-        for orientation, lines in self.line_minimization().items():
+        for orientation, lines in self.grid_lines.items():
             for line in lines:
                 cv2.line(self.img, (line.x1, line.y1), (line.x2, line.y2), (0, 255, 0), 2)
+
+    def draw_intersection_points(self):
+        for line in self.squares:
+            for square in line:
+                p1, p2, p3, p4 = square.get_points()
+                cv2.circle(self.img, p1, 3, (0, 0, 255), 5)
+                cv2.circle(self.img, p2, 3, (0, 0, 255), 5)
+                cv2.circle(self.img, p3, 3, (0, 0, 255), 5)
+                cv2.circle(self.img, p4, 3, (0, 0, 255), 5)
+
+    def draw_field(self):
+        knn = setup_classifier()
+        for line in self.squares:
+            print("+---+---+---+---+---+---+---+---+---+")
+            for i, square in enumerate(line):
+                predicted = classify(self.get_image(square), knn)
+                if predicted == 0:
+                    predicted = " "
+                print("|", predicted, "", end="")
+                if i == len(line) - 1:
+                    print("|")
 
     def show(self):
         cv2.imshow('Detected Lines.jpg', self.img)
         cv2.waitKey(0)
 
+    def get_image(self, square):
+        p1, p2, p3, p4 = square.get_points()
+        cut_border = 5
+        p1 = (p1[0] + cut_border+3, p1[1] + cut_border)
+        p4 = (p4[0] - cut_border, p4[1] - cut_border)
+        pts = np.array([[p1[0], p1[1]], [p4[0], p4[1]]])
+        r = cv2.boundingRect(pts)
+        #cv2.imshow(str(square), self.img[r[1]:r[1] + r[3], r[0]:r[0] + r[2]])
+        return self.img[r[1]:r[1] + r[3], r[0]:r[0] + r[2]]
+
 
 if __name__ == '__main__':
-    img = Image("sudokufield.jpg")
-    img.draw_lines()
+    img = Image("sudokuNice.jpg")
+    img.draw_field()
     img.show()
