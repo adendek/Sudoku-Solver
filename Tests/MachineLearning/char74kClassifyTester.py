e4c4a8e47441e93a03d70fe02828095ab5529c26
Alexis <ouksel.alexis@hotmail.fr>
2018-06-27 18:50:24 +0200

    try unit test

--- Tests/MachineLearning/char74kClassifyTester.py
+++ Tests/MachineLearning/char74kClassifyTester.py
@@ -1,36 +1,39 @@
 from unittest import TestCase
 from MachineLearning.char74kClassify import Char74kClassify
+from Common.Errors import InappropriateArgsError
 import numpy as np
 import cv2
 
 class TestChar74kClassify(TestCase):
-    def test_LoadModel(self):
-        pass
-
-    def test_LoadData(self):
-        pass
+    # def test_LoadModel(self):
+    #     pass
+    #
+    # def test_LoadData(self):
+    #     pass
 
     def test__Resizing(self):
         img = cv2.imread("..\..\DataSet\data\Sample004\img004-00002.png")
         expected = cv2.resize(img, (28,28))
         char74k = Char74kClassify()
-        self.assertEqual(char74k._Resizing(img), expected)
 
-    def test__ImgToArray(self):
-        img = cv2.imread(
-            r"C:\Users\Uporabnik\Desktop\fax\2.Letnik\python\Sudoku-Solver\DataSet\data\Sample004\img004-00002.png")
-        expected = np.array(img)
-        expected = expected.astype('float32')
-        expected /= 255
-        char74k = Char74kClassify()
-        self.assertEqual(char74k.ImgToArray(img), expected)
+        height, width, channels = char74k._Resizing(expected).shape
+        heightExpected, widthExpected, channelsExpected = char74k._Resizing(img).shape
+        self.assertEqual([height, width], [heightExpected, widthExpected])
+
 
     def test__Reshaping(self):
-        pass
-
-    def test_ClassifyImage(self):
-        img = cv2.imread(
-            r"C:\Users\Uporabnik\Desktop\fax\2.Letnik\python\Sudoku-Solver\DataSet\data\Sample004\img004-00002.png")
-        result = 5
+        img = cv2.imread("..\..\DataSet\data\Sample004\img004-00002.png")
         char74k = Char74kClassify()
-        self.assertEqual(char74k.ClassifyImage(img), result)
+        self.assertEqual(char74k._Reshaping(char74k._Resizing(char74k._ImgToArray(img))).shape, (1, 1, 28, 28, 3))
+
+    #
+    # def test_ClassifyImage(self):
+    #     img = cv2.imread("..\..\DataSet\data\Sample004\img004-00002.png")
+    #     result = 5
+    #     char74k = Char74kClassify()
+    #     img = char74k._Reshaping(char74k._Resizing(char74k._ImgToArray(img)))
+    #     self.assertEqual(char74k.ClassifyImage(img), result)
+    #     self.assertRaises(InappropriateArgsError, "img")
+    #     self.assertRaises(InappropriateArgsError, 3)
+    #     self.assertRaises(InappropriateArgsError, [])
+    #     self.assertRaises(InappropriateArgsError, 6.2)
