eca487273265097adeb7fc30dd31f5fc5dc57059
gasper <gasper.hribarrr@gmail.com>
2018-07-01 20:13:59 +0200

    test passes, everything is working

--- MachineLearning/char74kClassify.py
+++ MachineLearning/char74kClassify.py
@@ -1,21 +1,14 @@
-import os, cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn.utils import shuffle
-from sklearn.cross_validation import train_test_split
-
-from keras import backend as K
 from Common.validationFunctions import Validator
 from Common.Errors import InappropriateArgsError
+from keras import backend as K
+import numpy as np
+import keras
+import os
+import cv2
+
 K.set_image_dim_ordering('th')
 
-import keras
 
-
-
-
-# %%
 class Char74kClassify:
     def __init__(self):
         self.img_rows = 28
@@ -24,49 +17,37 @@ class Char74kClassify:
         # Define the number of classes
         self.num_classes = 10
         self.PATH = os.path.dirname(os.path.realpath(__file__))
-
         self._load_data()
 
     def _load_model(self, path):
         if Validator.is_type(path, str):
             if os.path.isfile(path):
-                print("Loading Model...")
                 with open(path, "r") as f:
                     json_str = f.read()
                 f.close()
                 self.model = keras.models.model_from_json(json_str)
                 return True
-            else:
-                raise InappropriateArgsError("Model not founded in directory")
-        else:
-            raise InappropriateArgsError("Input not valid")
-
+            raise InappropriateArgsError("Model not founded in directory")
+        raise InappropriateArgsError("Input not valid")
 
     def _load_data(self):
-
         self._load_model(self.PATH +"/../Model/model.json")
         self._load_file_weight(self.PATH + '/../Model/char74k.h5')
 
-
     def _load_file_weight(self, path):
         # Training
-
         if Validator.is_type(path, str):
             if os.path.isfile(path):
                 self.model.load_weights(path)
-                print("Model loaded")
                 return True
-            else:
-                raise InappropriateArgsError("Model not founded in directory")
-        else:
-            raise InappropriateArgsError("Input not valid")
+            raise InappropriateArgsError("Model not founded in directory")
+        raise InappropriateArgsError("Input not valid")
 
     def _resizing(self, img):
         if Validator.is_type(img, np.ndarray):
             img = cv2.resize(img, (28, 28))
             return img
-        else:
-            raise InappropriateArgsError("Input not valid")
+        raise InappropriateArgsError("Input not valid")
 
     def _img_to_array(self, img):
         if Validator.is_type(img, np.ndarray):
@@ -74,8 +55,7 @@ class Char74kClassify:
             img = img.astype('float32')
             img /= 255
             return img
-        else:
-            raise InappropriateArgsError("Input not valid")
+        raise InappropriateArgsError("Input not valid")
 
     def _reshaping(self, img):
         if isinstance(img, np.ndarray):
@@ -95,22 +75,11 @@ class Char74kClassify:
                     img = np.expand_dims(img, axis=0)
 
             return img
-        else:
-            raise InappropriateArgsError("Input not valid")
+        raise InappropriateArgsError("Input not valid")
 
     def classify_image(self, img):
         if Validator.is_type(img, np.ndarray):
-                img = self._resizing(img)
-                img = self._reshaping(img)
-
-                # Predicting the test image
-                return self.model.predict_classes(img)[0]
-        else:
-            raise InappropriateArgsError("Input not valid")
-
-
-if __name__ == '__main__':
-    model = Char74kClassify()
-    PATH = os.path.dirname(os.path.realpath(__file__))
-    img = cv2.imread("..\..\DataSet\data\Sample004\img004-00002.png")
-    print(model.classify_image(img))
+            img = self._resizing(img)
+            img = self._reshaping(img)
+            return self.model.predict_classes(img)[0]  # Predicting the test image
+        raise InappropriateArgsError("Input not valid")
--- Tests/MachineLearning/char74kClassifyTester.py
+++ Tests/MachineLearning/char74kClassifyTester.py
@@ -1,69 +1,54 @@
-from unittest import TestCase
+from Tests.ImageProcessing.extractSudokuFieldTester import PATH
 from MachineLearning.char74kClassify import Char74kClassify
 from Common.Errors import InappropriateArgsError
-
+from unittest import TestCase
 import cv2
 
-class TestChar74kClassify(TestCase):
 
+class TestChar74kClassify(TestCase):
+    def setUp(self):
+        self.char74k = Char74kClassify()
+        self.sample_img = cv2.imread(PATH + r"\..\..\SamplePictures\0-result.jpg")
+        
     def test__resizing(self):
-        char74k = Char74kClassify()
-        img = cv2.imread("..\..\DataSet\data\Sample004\img004-00002.png")
-        expected = cv2.resize(img, (28,28))
-        height, width, channels = char74k._resizing(expected).shape
-        heightExpected, widthExpected, channelsExpected = char74k._resizing(img).shape
-        self.assertEqual([height, width], [heightExpected, widthExpected])
+        expected = cv2.resize(self.sample_img, (28, 28))
+        height, width, channels = self.char74k._resizing(expected).shape
+        height_expected, width_expected, channels_expected = self.char74k._resizing(self.sample_img).shape
+        self.assertEqual([height, width], [height_expected, width_expected])
 
     def test_incorrect__resizing(self):
-        char74k = Char74kClassify()
-        self.assertRaises(InappropriateArgsError, lambda: char74k._resizing(" "))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._resizing(2))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._resizing(2.6))
+        self.assertRaises(InappropriateArgsError, self.char74k._resizing, " ")
+        self.assertRaises(InappropriateArgsError, self.char74k._resizing, 2)
+        self.assertRaises(InappropriateArgsError, self.char74k._resizing, 2.6)
 
     def test_incorrect__reshaping(self):
-        char74k = Char74kClassify()
-        self.assertRaises(InappropriateArgsError, lambda: char74k._reshaping(" "))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._reshaping(2))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._reshaping(2.6))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._reshaping([]))
+        self.assertRaises(InappropriateArgsError, self.char74k._reshaping, " ")
+        self.assertRaises(InappropriateArgsError, self.char74k._reshaping, 2)
+        self.assertRaises(InappropriateArgsError, self.char74k._reshaping, 2.6)
+        self.assertRaises(InappropriateArgsError, self.char74k._reshaping, [])
 
     def test_incorrect__classify_image(self):
-        char74k = Char74kClassify()
-        self.assertRaises(InappropriateArgsError, lambda: char74k.classify_image(" "))
-        self.assertRaises(InappropriateArgsError, lambda: char74k.classify_image(2))
-        self.assertRaises(InappropriateArgsError, lambda: char74k.classify_image(2.6))
+        self.assertRaises(InappropriateArgsError, self.char74k.classify_image, " ")
+        self.assertRaises(InappropriateArgsError, self.char74k.classify_image, 2)
+        self.assertRaises(InappropriateArgsError, self.char74k.classify_image, 2.6)
 
     def test__load_file_weight(self):
-        char74k = Char74kClassify()
-        self.assertEqual(char74k._load_file_weight('../../Model/char74k.h5'), True)
+        self.assertEqual(self.char74k._load_file_weight(PATH + '/../../Model/char74k.h5'), True)
 
     def test__load_model(self):
-        char74k = Char74kClassify()
-        self.assertEqual(char74k._load_model('../../Model/model.json'), True)
+        self.assertEqual(self.char74k._load_model(PATH + '/../../Model/model.json'), True)
 
     def test_incorrect__load_model(self):
-        char74k = Char74kClassify()
-        self.assertRaises(InappropriateArgsError, lambda: char74k._load_model(" "))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._load_model(2))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._load_model(2.6))
+        self.assertRaises(InappropriateArgsError, self.char74k._load_model, " ")
+        self.assertRaises(InappropriateArgsError, self.char74k._load_model, 2)
+        self.assertRaises(InappropriateArgsError, self.char74k._load_model, 2.6)
 
     def test_incorrect__load_file_weight(self):
-        char74k = Char74kClassify()
-        self.assertRaises(InappropriateArgsError, lambda: char74k._load_file_weight(" "))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._load_file_weight(2))
-        self.assertRaises(InappropriateArgsError, lambda: char74k._load_file_weight(2.6))
+        self.assertRaises(InappropriateArgsError, self.char74k._load_file_weight, " ")
+        self.assertRaises(InappropriateArgsError, self.char74k._load_file_weight, 2)
+        self.assertRaises(InappropriateArgsError, self.char74k._load_file_weight, 2.6)
 
     def test__reshaping(self):
-        char74k = Char74kClassify()
-        img = cv2.imread("..\..\DataSet\data\Sample004\img004-00002.png")
+        shape = self.char74k._reshaping(self.char74k._resizing(self.char74k._img_to_array(self.sample_img))).shape
+        self.assertEqual(shape, (1, 1, 28, 28, 3))
 
-        self.assertEqual(char74k._reshaping(char74k._resizing(char74k._img_to_array(img))).shape, (1, 1, 28, 28, 3))
-
-
-    def test_classify_image(self):
-        char74k = Char74kClassify()
-        img = cv2.imread("..\..\DataSet\data\Sample004\img004-00002.png")
-        result = 5
-
-        img = char74k._reshaping(char74k._resizing(char74k._img_to_array(img)))
-        self.assertEqual(char74k.classify_image(img), result)
